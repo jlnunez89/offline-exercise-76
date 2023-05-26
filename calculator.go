e99a7db0ce8b3bd7e16147447eaa8588e935ffa3
Jose Nunez de Caceres <jlnunez89@gmail.com>
2023-05-26 15:53:09 -0700

    mod init, add some failing tests

--- calculator.go
+++ calculator.go
@@ -1,5 +1,14 @@
-package main
+package calculator
 
-func main() {
+const DefaultInterest float64 = 0.01
 
+var lowerInterestLimits map[int]float64 = map[int]float64{
+	1000:  0.015,
+	5000:  0.02,
+	10000: 0.025,
+	50000: 0.03,
+}
+
+func CalculateInterest(balance float64) float64 {
+	return DefaultInterest
 }
--- calculator_test.go
+++ calculator_test.go
@@ -1,7 +1,52 @@
-package main
+package calculator
 
-import "testing"
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 func TestCalculateInterest(t *testing.T) {
-	t.Errorf("Test failed")
+	t.Run("Negative balance should return 0", func(t *testing.T) {
+		// arrange
+		negativeBalance := -1.0
+		expectedInterest := 0.0
+
+		// act
+		interest := CalculateInterest(negativeBalance)
+
+		// assert
+		assert.Equal(t, expectedInterest, interest)
+	})
+
+	t.Run("Zero balance should return 0", func(t *testing.T) {
+		// arrange
+		zeroBalance := 0.0
+		expectedInterest := 0.0
+
+		// act
+		interest := CalculateInterest(zeroBalance)
+
+		// assert
+		assert.Equal(t, expectedInterest, interest)
+	})
+
+	t.Run("Test balances between 0 and 1000", func(t *testing.T) {
+		// arrange
+		epsilon := 0.01 // since one cent is normally the smallest unit of currency
+		nonZeroBalance := 0.0 + epsilon
+		lessThan1000Balance := 1000.0 - epsilon
+		expectedInterestPercentage := DefaultInterest
+
+		for balance := nonZeroBalance; balance < lessThan1000Balance; balance += epsilon {
+			// act
+			calculatedInterest := CalculateInterest(balance)
+			expectedInterest := math.RoundToEven(balance*expectedInterestPercentage*100.0) / 100.0 // round to nearest even number because finance
+
+			// assert
+			require.Equal(t, expectedInterest, calculatedInterest)
+		}
+	})
 }
